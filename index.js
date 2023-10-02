69167f3288482bbbdd458dbce4555791062fe466
QueenIce90 <Isamargonzalez90@gmail.com>
2023-10-02 05:28:08 -0400

    short message briefly explaining the changes saved in this commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // Assuming the game field width is 400px and the dodger is 40px wide
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+
+
+ 
+
+
